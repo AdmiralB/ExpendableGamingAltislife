42467c4dee5da85e8898dccee740394ce81fdecb
AdmiralB <Anders.i.gundersen1993@gmail.com>
2015-01-16 12:53:21 +0100

    Create Functions.h

---
+++ Functions.h
@@ -0,0 +1,12 @@
+class Items 
+2 { 
+3     file = "core\items"; 
+4     class pickaxeUse {}; 
+5     class lockpick {}; 
+6     class spikeStrip {}; 
+7     class jerryRefuel {}; 
+8     class weed {}; 
+9     class cocaine {}; 
+10     class heroin {}; 
+11     class shroom {}; 
+12 }; 
